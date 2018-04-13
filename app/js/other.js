ed707fa4a3b099e1d5dbb2ca653baeee014be38a
vikasya <vikasya@cybage.com>
2018-04-13 16:35:15 +0530

    event dispatcher added

--- app/js/other.js
+++ app/js/other.js
@@ -1,5 +1,20 @@
-console.log('Version -----'+ gQ.version());
+console.log('Version -----' + gQ.version());
 
-gQ.start = function() {
+gQ.start = function () {
     console.log(gQ('.heading'));
-}
+
+    // gQ('span', '#li1').text('ram');
+
+    // console.log(gQ('span', '#li1').dom());
+
+    var ticker = gQ.ticker();
+    ticker.add(100, 4, function () {
+        // console.log('I am called.');
+    });
+
+
+    // ticker.addEvent('tick', function (e) {
+    //     console.log('a tick just happend' + e.target, e.type);
+    // });
+
+};
--- app/library/gq.js
+++ app/library/gq.js
@@ -62,35 +62,95 @@
         return version;
     };
 
+    gQ.ticker = function () {
+        return Ticker.getInstance();
+    };
+
     gQ.ready(function () {
 
         if ('jQuery' in scope) {
-            q = new JQueryAdapter(scope.jQuery);
+            q = QueryFacade.create(JQueryAdapter, scope.jQuery, doc);
             gQ.start();
         } else if (doc.querySelectorAll && doc.querySelectorAll('body:first-of-type')) {
-            q = new NativeQuery();
+            q = QueryFacade.create(NativeQuery, null, doc);
             gQ.start();
         } else {
             gQ.loadJs('./js/sizzle.min.js', function () {
-                q = new SizzleAdapter(Sizzle);
+                q = QueryFacade.create(SizzleAdapter, Sizzle, doc);
                 gQ.start();
             });
         }
     });
 
-    NativeQuery = function () {};
+    QueryFacade = function (adapter) {
+        var dom = function () {
+                return adapter.context;
+            },
+            query = function (selector, context) {
+                return QueryFacade(adapter.query(selector, context));
+            },
+            text = function (val) {
+                return adapter.text(val);
+            };
 
-    NativeQuery.prototype.query = function (selector, context) {
-        context = context || doc;
-        return gQ.toArray(context.querySelectorAll(selector));
+        return {
+            dom: dom,
+            query: query,
+            text: text
+        };
+
     };
 
-    SizzleAdapter = function (lib) {
+    QueryFacade.create = function (adapter, lib, context) {
+        return QueryFacade(new adapter(lib, context));
+    };
+    // QueryFacade.prototype.dom = function() {
+    //     return this.adapter.context;
+    // };
+
+    // QueryFacade.prototype.query = function (selector, context) {
+    //     return new QueryFacade(this.adapter.query(selector, context));
+    // };
+
+    // QueryFacade.prototype.text = function (val) {
+    //     return this.adapter.text(val);
+    // };
+
+    NativeQuery = function (lib, context) {
+        this.context = context;
+    };
+
+    NativeQuery.prototype.query = function (selector, context) {
+        context = context || this.context;
+        return new NativeQuery(gQ.toArray(context.querySelectorAll(selector)));
+    };
+
+    NativeQuery.prototype.text = function (val) {
+        var innerText = this.context[0].innerText === undefined ? 'textContent' : 'innerText';
+        for (var item in this.context) {
+            this.context[item][innerText] = val;
+        }
+        return val;
+    };
+
+
+    SizzleAdapter = function (lib, context) {
         this.lib = lib;
+        this.context = context;
+
     };
     SizzleAdapter.prototype.query = function (selector, context) {
         context = context || doc;
-        return gQ.toArray(this.lib(selector, context));
+        return new SizzleAdapter(this.lib, gQ.toArray(this.lib(selector, context)));
+    };
+
+    SizzleAdapter.prototype.text = function (val) {
+
+        var innerText = this.context[0].innerText === undefined ? 'textContent' : 'innerText';
+        for (var item in this.context) {
+            this.context[item][innerText] = val;
+        }
+        return val;
     };
 
     JQueryAdapter = function (lib, context) {
@@ -100,12 +160,130 @@
     };
     JQueryAdapter.prototype.query = function (selector, context) {
         context = context || doc;
-        return this.lib(selector, context).get();
+        return new JQueryAdapter(this.lib, this.lib(selector, context).get());
     };
 
     JQueryAdapter.prototype.text = function (val) {
-      return this.target.text(val);
+        return this.target.text(val);
     };
+
+    var Ticker = (function () {
+        var instance;
+
+        function create() {
+            // props 
+            var intervalId,
+                curentInterval = 0,
+                index = 0,
+                maxInterval = 0,
+                sensitivity = 100,
+                methods = {},
+                api;
+            // methods of singlton
+            // public methods 
+            function add(interval, times, callback, name) {
+                var realInterval = interval - interval % sensitivity;
+                maxInterval = Math.max(realInterval, maxInterval);
+                name = name || (++index);
+
+                if (!methods[realInterval]) {
+                    methods[realInterval] = {};
+                }
+                methods[realInterval][name] = {
+                    times: times,
+                    callback: callback,
+                    interval: interval
+                };
+
+                start();
+
+            }
+            // private methods 
+
+            function start() {
+
+                if (!intervalId) {
+                    intervalId = setInterval(runInterval, sensitivity);
+                }
+
+            }
+
+            function runInterval() {
+                api.dispatchEvent({
+                    type: 'pretick',
+                    target: api
+                });
+                curentInterval = curentInterval % maxInterval;
+                curentInterval += sensitivity;
+
+                for (var interval in methods) {
+                    if (curentInterval % interval === 0) {
+                        processIntervalGroup(methods[interval]);
+                    }
+                }
+
+                api.dispatchEvent({
+                    type: 'tick',
+                    target: api
+                });
+            }
+
+            function processIntervalGroup(group) {
+                var item;
+                for (var name in group) {
+                    item = group[name];
+                    item.callback()
+                    if (item.times === 0) {
+                        delete group[name];
+                    } else {
+                        --item.times;
+                    }
+                }
+            }
+
+            api = {
+                add: add
+            };
+
+            EventDispatcher(api);
+            return api;
+        }
+
+        return {
+            getInstance: function () {
+                if (!instance) {
+                    instance = create();
+                }
+                return instance;
+            }
+        };
+    })();
+
+    function EventDispatcher(o) {
+        var list = {};
+        o.addEvent = function (type, listner) {
+            if (!list[type]) {
+                list[type] = [];
+            }
+            if (list[type].indexOf(listner) === -1) {
+                list[type].push(listner);
+            }
+        };
+
+        o.dispatchEvent = function (e) {
+            var a = list[e.type];
+            if (a) {
+                if (!e.target) {
+                    e.target = this;
+                }
+                for (var i in a) {
+                    a[i](e);
+                }
+            }
+        };
+    }
+
+
 
     if (!scope.gQ) {
         scope.gQ = gQ;
